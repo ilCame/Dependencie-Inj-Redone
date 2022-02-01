af78679e87ffc8cede0737c21ea04205037980a5
ilCame <ale.camer'pv@gmail.com>
2022-02-01 14:24:20 +0100

    first commit

---
+++ src/main/java/com/dependency/injection/rebuild/dir/DirApplication.java
@@ -0,0 +1,13 @@
+package com.dependency.injection.rebuild.dir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dependency/injection/rebuild/dir/DirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dependency.injection.rebuild.dir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
